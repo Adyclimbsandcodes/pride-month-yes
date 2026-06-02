641cad4a798670fc9740f372b2c7e75cf5366d9d
Adeline Wright <adeline.wright1993@gmail.com>
2026-06-02 19:49:04 -0400

    Week 1 HW.2
    This codes asks the user for any positive integer, and that denotes the number of 2D stairs printed that ascend towards the right.

--- mario.c
+++ mario.c
@@ -1,18 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
 
-void left(int k, int steps);
-void right(int k);
+void decrement(int k, int steps);
+void increment(int k);
 int main(void)
 {
     int height;
-    // check for valid submission, must be a number greater than 0 and less than 9
     while (true)
     {
-        height = get_int("pick a number between 1 and 8 inclusive: ");
-        if (height <= 0 || height > 8)
+        // prompt user to give height value
+        height = get_int("height: ");
+
+        // check for valid input
+        if (height <= 0)
         {
-            printf("invalid input. ");
+            printf("invalid answer. please give a value greater than 0:\n");
+            // prompt user to give height value
             continue;
         }
         else
@@ -20,35 +23,27 @@ int main(void)
             break;
         }
     }
-    // set conditions to print based on height given using for loops
     for (int i = 0; i < height; i++)
     {
-        // left stairs
-        left(i, height);
-        // right stairs
-        right(i);
-        // new line for a cleaner look after all stairs printed
+        // decrement pyramid
+        decrement(i, height);
+
+        // increment pyramid
+        increment(i);
+
         printf("\n");
     }
 }
 
-// left stairs
-void left(int k, int steps)
+void decrement(int k, int steps)
 {
     for (int j = 0; j < steps - (k + 1); j++)
     {
         printf(" ");
     }
-    for (int j = 0; j < k + 1; j++)
-    {
-        printf("#");
-    }
-    // space between stairs sets
-    printf("  ");
 }
 
-// right stairs
-void right(int k)
+void increment(int k)
 {
     for (int j = 0; j < k + 1; j++)
     {
